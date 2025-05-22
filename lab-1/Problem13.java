af8a358dd285123bb291f04534340fc5e37996f2
clickTwice26 <shagatoc@gmail.com>
2025-05-23 00:26:47 +0600

    Add Problem13 class for user information input and display

---
+++ lab-1/Problem13.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Problem13 {
+    public static void main(String args[]){
+        System.out.println("Insert Your Information");
+        Scanner input = new Scanner(System.in);
+        String full_name, student_id, hometown, description;
+        char section;
+
+        System.out.print("Full Name : ");
+        full_name = input.nextLine();
+        System.out.print("Student Id : ");
+        student_id = input.nextLine();
+        System.out.print("Section : ");
+        section = input.next().charAt(0);
+        System.out.print("Hometown : ");
+        hometown = input.next();
+        input.nextLine();
+        
+        System.out.print("Description : ");
+        
+        description = input.nextLine();
+
+
+        System.out.printf(
+            "Full Name : %s\nSection : %c\nStudent Id: %s\nHometown: %s\nDescription: %s\n",full_name,section,student_id,hometown,description
+        );
+
+
+
+
+    
+    }
+}
